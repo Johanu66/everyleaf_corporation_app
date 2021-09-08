53412beac8c0285a53bebc62c56faf69c6eb7ea0
Johanu GANDONOU <johanugandiniu@gmail.com>
2021-09-08 10:04:59 +0100

    L'étape 4 est prete

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 1.times do |n|
-    name = Faker::Games::Pokemon.name
-    email = Faker::Internet.email
-    password = "password"
+    name = "Johanu"
+    email = "johanugandonou@gmail.com"
+    password = "123456"
     User.create!(name: name,
                  email: email,
                  password: password,
+                 admin: true
                  )
 end
